46a30e3a749b4866e479941019f75e749e742823
joshua-sigston <joshua.sigton.com>
2024-11-10 18:04:41 -0800

    restart to fix eslint and propType validations

---
+++ src/features/menu/MenuItem.jsx
@@ -0,0 +1,32 @@
+import { formatCurrency } from '../../utilities/helpers';
+import PropTypes from 'prop-types';
+
+MenuItem.propTypes = {
+  pizza: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    unitPrice: PropTypes.number.isRequired,
+    ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+    soldOut: PropTypes.bool.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+function MenuItem({ pizza }) {
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  console.log(pizza);
+  return (
+    <li>
+      <img src={imageUrl} alt={name} />
+      <div>
+        <p>{name}</p>
+        <p>{ingredients.join(', ')}</p>
+        <div>
+          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default MenuItem;
